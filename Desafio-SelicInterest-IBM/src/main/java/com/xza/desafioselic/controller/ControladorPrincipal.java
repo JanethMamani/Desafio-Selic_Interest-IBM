eca389e250ac012e9aa5d1e0e6b76dc295ec0224
Janeth Mamani Sacari <janeth@ibm.com>
2021-10-14 14:45:03 -0300

    App com endpoint findAll testado

--- Desafio-SelicInterest-IBM/src/main/java/com/xza/desafioselic/controller/ControladorPrincipal.java
+++ Desafio-SelicInterest-IBM/src/main/java/com/xza/desafioselic/controller/ControladorPrincipal.java
@@ -3,27 +3,31 @@ package com.xza.desafioselic.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.xza.desafioselic.entidades.TaxaSelic;
 import com.xza.desafioselic.services.ServicoConversao;
 
 @Controller
+@RequestMapping(value = "/taxas")
 public class ControladorPrincipal {
 	
-	private static final String PAGINA_PRINCIPAL = "principal";
+	//private static final String PAGINA_PRINCIPAL = "principal";
 	private static final String JSON_TAXA_URL = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.4390/dados?formato=json";
 	
 	@Autowired
 	private ServicoConversao servicoConversao;
 	
+	//Inicial
 	@GetMapping
-	public String principal(final Model modelo) {
-		List<TaxaSelic> taxas = (List<TaxaSelic>) servicoConversao.parse(JSON_TAXA_URL);
-		modelo.addAllAttributes(taxas);
-		return PAGINA_PRINCIPAL;
+	public ResponseEntity<List<TaxaSelic>> encontrarTodos(final Model modelo){
+		List<TaxaSelic> taxasS = (List<TaxaSelic>) servicoConversao.parse(JSON_TAXA_URL);
+		modelo.addAllAttributes(taxasS);
+		return ResponseEntity.ok(taxasS);
 	}
 
 }
